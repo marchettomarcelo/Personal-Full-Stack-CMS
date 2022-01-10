2bcef86ceef185c34d022efe0fca71c0f65587c6
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-01-10 15:56:40 -0300

    minor changes resolved

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../fe-cms/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
